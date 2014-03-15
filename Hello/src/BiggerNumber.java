ed314bc61a86d4a8864e46e6f7b86a5c33632738
Naveen Kumar <miryalan95@gmail.com>
2014-03-15 18:06:52 -0500

    hello

---
+++ Hello/src/BiggerNumber.java
@@ -0,0 +1,14 @@
+
+public class BiggerNumber {
+
+	public static void main(String[] args) {
+		int n1=10;
+		int n2=20;
+		if(n1>n2)
+			System.out.println(n1+"is bigger than"+n2);
+		else
+			System.out.println(n2+"is bigger than"+n1);
+
+	}
+
+}
